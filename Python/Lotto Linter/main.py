ab052c2ef507fd05050117e46c6babe53142ea52
KillerRhys <56098017+KillerRhys@users.noreply.github.com>
2024-04-13 12:54:07 -0400

    Cleanup & Comments

--- Python/Lotto Linter/main.py
+++ Python/Lotto Linter/main.py
@@ -1,38 +1,43 @@
-""" Lotto Linter Ver: 1.0
+""" Lotto Linter
     Coded by TechGYQ
     www.MythosWorks.com
     OC:2022.07.23-1049 """
-
-
+import datetime
 import tkinter
 import urllib.request
 import pandas as pd
 
 
 # VARS
-version = 1.0
+version = 1.1
+current_year = datetime.datetime.today().year
+target_year = current_year - 5
 
 
+# Get csv data for last five years of drawings.
 def get_nums():
-    mega_millions_url = 'https://www.texaslottery.com/export/sites/lottery/Games/Mega_Millions/Winning_Numbers/megamillions.csv'
+    mega_millions_url = ('https://www.texaslottery.com/export/sites/lottery/Games/Mega_Millions/Winning_Numbers/'
+                         'megamillions.csv')
     power_ball_url = 'https://www.texaslottery.com/export/sites/lottery/Games/Powerball/Winning_Numbers/powerball.csv'
-    urllib.request.urlretrieve(mega_millions_url, 'data/mm2022.csv')
-    urllib.request.urlretrieve(power_ball_url, 'data/pb2022.csv')
-    mega_millions_data = pd.read_csv('data/mm2022.csv')
-    power_ball_data = pd.read_csv('data/pb2022.csv')
+    urllib.request.urlretrieve(mega_millions_url, f'data/mm{current_year}.csv')
+    urllib.request.urlretrieve(power_ball_url, f'data/pb{current_year}.csv')
+    mega_millions_data = pd.read_csv(f'data/mm{current_year}.csv')
+    power_ball_data = pd.read_csv(f'data/pb{current_year}.csv')
     header_list = ['Lotto', 'Month', 'Day', 'Year', 'Num1', 'Num2', 'Num3', 'Num4', 'Num5', 'MegaPower', 'Multiply']
-    mega_millions_data.to_csv('data/mm2022.csv', header=header_list, index=False)
-    power_ball_data.to_csv('data/pb2022.csv', header=header_list, index=False)
-    mega_millions = pd.read_csv('data/mm2022.csv')
-    mega_million = mega_millions[mega_millions['Year'] >= 2016]
-    power_balls = pd.read_csv('data/pb2022.csv')
-    power_ball = power_balls[power_balls['Year'] >= 2016]
+    mega_millions_data.to_csv(f'data/mm{current_year}.csv', header=header_list, index=False)
+    power_ball_data.to_csv(f'data/pb{current_year}.csv', header=header_list, index=False)
+    mega_millions = pd.read_csv(f'data/mm{current_year}.csv')
+    mega_million = mega_millions[mega_millions['Year'] >= target_year]
+    power_balls = pd.read_csv(f'data/pb{current_year}.csv')
+    power_ball = power_balls[power_balls['Year'] >= target_year]
 
+    # Create list for Frequently used | Rarely used numbers.
     mega_list_common = []
     mega_list_rare = []
     power_list_common = []
     power_list_rare = []
 
+    # Use Pandas to build and fill list by most and least used.
     fetch = ['Num1', 'Num2', 'Num3', 'Num4', 'Num5', 'MegaPower']
     for item in fetch:
         s = mega_million[item].value_counts()
@@ -56,14 +61,15 @@ def get_nums():
     power_label['text'] = f'Power Ball: {power_list_common} || {power_list_rare}'
 
 
-# Display
+# Display GUI with tkinter.
 display = tkinter.Tk()
 display.title(f'Lotto Linter {version}')
 display.configure(bg='black')
 display.minsize(width=600, height=200)
 mega_label = tkinter.Label(text='Mega Millions!', font=('Arial', 24, 'bold'), bg='black', fg='teal')
 power_label = tkinter.Label(text='Power Ball', font=('Arial', 24, 'bold'), bg='black', fg='teal')
-magic_button = tkinter.Button(text='Got dem nums?', command=get_nums, bg='black', fg='purple', font=('Arial', 24, 'bold'))
+magic_button = tkinter.Button(text='Got dem nums?', command=get_nums, bg='black', fg='purple',
+                              font=('Arial', 24, 'bold'))
 mega_label.pack()
 power_label.pack()
 magic_button.pack()
